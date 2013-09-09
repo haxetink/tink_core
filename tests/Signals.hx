a7d74d8f01375a889248a8823d8132b6696413a5
back2dos <back2dos@gmail.com>
2013-09-09 23:35:03 +0200

    More Signal tests.

--- tests/Signals.hx
+++ tests/Signals.hx
@@ -1,6 +1,7 @@
 package ;
 
 using tink.core.Signal;
+using tink.core.Future;
 using tink.core.Callback;
 
 class Signals extends Base {
@@ -21,6 +22,7 @@ class Signals extends Base {
 		handlers1.invoke('bar');
 		assertEquals('foo', value);
 	}
+	
 	function testJoinNoGather() {
 		var s = signal1.join(signal2, false);
 		assertEquals(0, handlers1.length);
@@ -52,6 +54,7 @@ class Signals extends Base {
 		assertEquals(0, handlers1.length);
 		assertEquals(0, handlers2.length);		
 	}
+	
 	function testJoinGather() {
 		var s = signal1.join(signal2);
 		
@@ -142,4 +145,127 @@ class Signals extends Base {
 		
 		assertEquals(0, handlers1.length);
 	}
+	
+	function testFlatMap() {
+		var mapCalls = 0,
+			out = '',
+			inQueueData = [for (i in 1...1000) Std.string(i)],
+			inQueue = [];
+		
+		function make() {
+			var f = Future.create();
+			var data = inQueueData.shift();
+			inQueue.push(function () f.invoke(data));
+			return f.asFuture();
+		}
+		function step() 
+			inQueue.shift()();
+		
+		var s = signal1.flatMap(function (v1) { mapCalls++; return make().map(function (v2) return v1 + v2); });
+		
+		assertEquals(1, handlers1.length);
+		
+		var calls = 0;
+		
+		var link1 = s.when(function () calls++),
+			link2 = s.when(function () calls++),
+			link3 = s.when(function (v) out += v);
+		
+		assertEquals(1, handlers1.length);
+
+		assertEquals(0, calls);
+		assertEquals(0, mapCalls);
+		
+		handlers1.invoke('1');
+		
+		assertEquals(0, calls);
+		assertEquals(1, mapCalls);
+		
+		handlers1.invoke('2');
+		
+		assertEquals(0, calls);
+		assertEquals(2, mapCalls);
+		
+		assertEquals('', out);
+		
+		step();
+		
+		assertEquals(2, calls);
+		assertEquals(2, mapCalls);
+		
+		assertEquals('11', out);
+		
+		handlers1.invoke('3');
+		
+		assertEquals(2, calls);
+		assertEquals(3, mapCalls);
+		
+		step();
+		step();
+		
+		assertEquals(6, calls);
+		assertEquals(3, mapCalls);
+		assertEquals('112233', out);
+	}
+	
+	function testFlatMapNoGather() {
+		var mapCalls = 0,
+			out = '',
+			inQueueData = [for (i in 1...1000) Std.string(i)],
+			inQueue = [];
+		
+		function make() {
+			var f = Future.create();
+			var data = inQueueData.shift();
+			inQueue.push(function () f.invoke(data));
+			return f.asFuture();
+		}
+		function step() 
+			inQueue.shift()();
+		
+		var s = signal1.flatMap(function (v1) { mapCalls++; return make().map(function (v2) return v1 + v2); }, false);
+		
+		assertEquals(0, handlers1.length);
+		
+		var calls = 0;
+		
+		var link1 = s.when(function () calls++),
+			link2 = s.when(function () calls++),
+			link3 = s.when(function (v) out += v);
+		
+		assertEquals(3, handlers1.length);
+
+		assertEquals(0, calls);
+		assertEquals(0, mapCalls);
+		
+		handlers1.invoke('1');
+		
+		assertEquals(0, calls);
+		assertEquals(3, mapCalls);
+		
+		handlers1.invoke('2');
+		
+		assertEquals(0, calls);
+		assertEquals(6, mapCalls);
+		
+		assertEquals('', out);
+		
+		step();
+		
+		assertEquals(1, calls);
+		assertEquals(6, mapCalls);
+		assertEquals('', out);
+		
+		step();
+		
+		assertEquals(2, calls);
+		assertEquals(6, mapCalls);
+		assertEquals('', out);
+		
+		step();
+		
+		assertEquals(2, calls);
+		assertEquals(6, mapCalls);
+		assertEquals('13', out);
+	}	
 }
