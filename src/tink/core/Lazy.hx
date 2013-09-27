dc5f4276549fc0915361e6af979fce6fedd49d3b
back2dos <back2dos@gmail.com>
2013-09-27 12:10:18 +0200

    Added Lazy.

---
+++ src/tink/core/Lazy.hx
@@ -0,0 +1,24 @@
+package tink.core;
+
+abstract Lazy<T>(Void->T) {
+	
+	inline function new(r) this = r;
+	
+	@:to public inline function get():T
+		return (this)();
+			
+	@:from static function ofFunc<T>(f:Void->T) {
+		var result = null;
+		return new Lazy(function () {
+			if (f != null) {
+				var f2 = f;
+				f = null;
+				result = f2();
+			}
+			return result;
+		});
+	}
+	
+	@:from static inline function ofConst<T>(c:T) 
+		return new Lazy(function () return c);
+}	
