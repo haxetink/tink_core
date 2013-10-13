84e9036befe9a781d83264bacb54db2c7c923980
back2dos <back2dos@gmail.com>
2013-10-13 23:13:21 +0200

    Don't assume dom on js,

--- tests/Run.hx
+++ tests/Run.hx
@@ -12,7 +12,20 @@ class Run {
 		new Refs(),
 		new Pairs()
 	];
-	static function main() {		
+	static function main() {	
+		#if js//works for nodejs and browsers alike
+		var buf = [];
+		TestRunner.print = function (s:String) {
+			var parts = s.split('\n');
+			if (parts.length > 1) {
+				parts[0] = buf.join('') + parts[0];
+				buf = [];
+				while (parts.length > 1)
+					untyped console.log(parts.shift());
+			}
+			buf.push(parts[0]);
+		}
+		#end		
 		var r = new TestRunner();
 		for (c in tests)
 			r.add(c);
