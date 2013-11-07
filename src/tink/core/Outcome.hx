ec47aef283eaa8f8945b68221ba7978ceb7a6a29
back2dos <back2dos@gmail.com>
2013-11-07 14:52:18 +0100

    Add flatMap with trampolines. Boiiiiiiing!

--- src/tink/core/Outcome.hx
+++ src/tink/core/Outcome.hx
@@ -1,6 +1,7 @@
 package tink.core;
 
 import haxe.ds.Option;
+import tink.core.Either;
 
 enum Outcome<Data, Failure> {//TODO: turn into abstract when this commit is released: https://github.com/HaxeFoundation/haxe/commit/e8715189fc055220f2f33a06c5e1331c96310a88
 	Success(data:Data);
@@ -36,7 +37,8 @@ class OutcomeTools {
 					Failure('Some value expected but none found in ' + pos.fileName + '@line ' + pos.lineNumber);
 			}
 	
-	static public inline function orUse<D, F>(outcome: Outcome<D, F>, fallback: D ) 
+	//TODO: orTry and orUse should probably become Lazy to allow for short-circuit evaluation
+	static public inline function orUse<D, F>(outcome: Outcome<D, F>, fallback: D) 
 		return
 			switch (outcome) {
 				case Success(data): data;
@@ -73,4 +75,35 @@ class OutcomeTools {
 				default: false;
 			}
 	
+	static public function flatMap<DIn, FIn, DOut, FOut>(o:Outcome<DIn, FIn>, mapper:OutcomeMap<DIn, FIn, DOut, FOut>):Outcome<DOut, FOut> {
+		return mapper.apply(o);
+	}
+}
+
+private abstract OutcomeMap<DIn, FIn, DOut, FOut>({ f: Outcome<DIn, FIn>->Outcome<DOut, FOut> }) {
+	function new(f) this = { f: f };
+	public function apply(o) 
+		return this.f(o);
+		
+	@:from static function withSameError<In, Out, Error>(f:In->Outcome<Out, Error>):OutcomeMap<In, Error, Out, Error> {
+		return new OutcomeMap(function (o)
+			return switch o {
+				case Success(d): f(d);
+				case Failure(f): Failure(f);
+			}
+		);
+	}
+	
+	@:from static function withEitherError<DIn, FIn, DOut, FOut>(f:DIn->Outcome<DOut, FOut>):OutcomeMap<DIn, FIn, DOut, Either<FIn, FOut>> {
+		return new OutcomeMap(function (o)
+			return switch o {
+				case Success(d): 
+					switch f(d) {
+						case Success(d): Success(d);
+						case Failure(f): Failure(Right(f));
+					}
+				case Failure(f): Failure(Left(f));
+			}
+		);		
+	}
 }
--- tests/Outcomes.hx
+++ tests/Outcomes.hx
@@ -23,4 +23,20 @@ class Outcomes extends Base {
 		assertFalse(Success(-4).equals(4));
 		assertFalse(Failure(4).equals(4));
 	}
+	
+	function testFlatMap() {
+		var outcomes = [
+			Success(5), 
+			Failure('error')
+		];
+		function eq<A>(exp:A, found:A)
+			assertTrue(Type.enumEq(exp, found));
+			
+		
+		eq(Success(3), outcomes[0].flatMap(function (x) return Success(x - 2)));
+		eq(Failure('error'), outcomes[1].flatMap(function (x) return Success(x - 2)));
+		
+		eq(Failure(Right(7)), outcomes[0].flatMap(function (x) return Failure(x + 2)));
+		eq(Failure(Left('error')), outcomes[1].flatMap(function (x) return Failure(x + 2)));
+	}
 }
