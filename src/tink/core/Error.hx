c97a3397e339738da9045f1dd5043168746272be
back2dos <back2dos@gmail.com>
2014-06-05 20:25:02 +0200

    Common error codes.

--- src/tink/core/Error.hx
+++ src/tink/core/Error.hx
@@ -8,12 +8,40 @@ typedef Pos =
 	#end
 
 //TODO: there's huge overlap with haxe.macro.Error
-class Error {
+typedef Error = TypedError<Dynamic>;
+
+@:enum abstract ErrorCode(Int) from Int to Int {
+	var BadRequest = 400;
+	var Unauthorized = 401;
+	var PaymentRequired = 402;
+	var Forbidden = 403;
+	var NotFound = 404;
+	var MethodNotAllowed = 405;
+	var Gone = 410;
+	var NotAcceptable = 406;
+	var Timeout = 408;
+	var Conflict = 409;
+	var OutOfRange = 416;
+	var ExpectationFailed = 417;
+	var I_am_a_Teapot = 418;
+	var AuthenticationTimeout = 419;
+
+	var InternalError = 500;
+	var NotImplemented = 501;
+	var ServiceUnavailable = 503;
+	var InsufficientStorage = 507;
+	var BandwidthLimitExceeded = 509;
+
+}
+
+class TypedError<T> {
 	public var message(default, null):String;//It might make sense for the message to be lazy
-	public var data(default, null):Dynamic;
+	public var code(default, null):Int;
+	public var data(default, null):T;
 	public var pos(default, null):Null<Pos>;
 	
-	public function new(message, ?pos) {
+	public function new(?code:ErrorCode = InternalError, message, ?pos) {
+		this.code = code;
 		this.message = message;
 		this.pos = pos;
 	}
@@ -44,8 +72,8 @@ class Error {
 				throw this;
 			#end
 		
-	static public function withData(message:String, data:Dynamic, ?pos:Pos) {
-		var ret = new Error(message, pos);
+	static public function withData(?code:Int = 500, message:String, data:Dynamic, ?pos:Pos) {
+		var ret = new Error(code, message, pos);
 		ret.data = data;
 		return ret;
 	}
