25d2b44d4f99840f67c687b79df3e5f4267396d9
Kevin Leung <kevinresol@gmail.com>
2016-12-04 19:54:18 +0800

    Make sequence readonly

--- src/tink/core/Sequence.hx
+++ src/tink/core/Sequence.hx
@@ -1,8 +1,12 @@
 package tink.core;
 
-@:forward
+@:forward(concat, copy, filter, indexOf, iterator, join, lastIndexOf, map, slice, toString)
 abstract Sequence<T>(Array<T>) from Array<T> to Array<T> {
   @:from
   public static inline function ofSingle<T>(v:T):Sequence<T>
     return [v];
+  
+  @:arrayAccess
+  public inline function get(i:Int)
+    return this[i];
 }
