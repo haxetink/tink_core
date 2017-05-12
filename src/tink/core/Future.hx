8d33a266bb87911eb219366e5063a91b97c5a401
Kevin Leung <kevinresol@gmail.com>
2017-05-12 10:50:35 +0800

    Add NEVER constants

--- src/tink/core/Future.hx
+++ src/tink/core/Future.hx
@@ -5,6 +5,7 @@ using tink.CoreApi;
 abstract Future<T>(FutureObject<T>) from FutureObject<T> to FutureObject<T> {
   
   public static var NOISE:Future<Noise> = Future.sync(Noise);
+  public static var NEVER:Future<Dynamic> = Future.async(function(_){});
 
   public inline function new(f:Callback<T>->CallbackLink) 
     this = new SimpleFuture(f);  
