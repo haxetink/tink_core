d58c7b37a78df3697fd78143af856444456dfd19
Evgeny Pavlovskiy <evgeny.pavlovskiy@doclerholding.com>
2018-05-25 15:58:11 +0200

    tests/Lazies.hx: Untabify indents

--- tests/Lazies.hx
+++ tests/Lazies.hx
@@ -3,33 +3,34 @@ package;
 using tink.CoreApi;
 
 class Lazies extends Base {
-	function testConst() {
-		var counter = 0;
+  function testConst() {
+    var counter = 0;
 
-		function double(x):Int {
-			++counter;
-			return x * 2;
-		}
+    function double(x):Int {
+      ++counter;
+      return x * 2;
+    }
 
-		function lazyDouble(x):Lazy<Int> {
-			++counter;
-			return x * 2;
-		}
+    function lazyDouble(x):Lazy<Int> {
+      ++counter;
+      return x * 2;
+    }
 
-		var i:Lazy<Int> = 7;
+    var i:Lazy<Int> = 7;
 
-		var j = i.map(double);
-		assertEquals(0, counter);
-		assertEquals(j.get(), 14);
-		j.get();
-		assertEquals(1, counter);
+    var j = i.map(double);
+    assertEquals(0, counter);
+    assertEquals(j.get(), 14);
+    j.get();
+    assertEquals(1, counter);
 
-		counter = 0;
+    counter = 0;
 
-		var k = i.flatMap(lazyDouble);
-		assertEquals(0, counter);
-		assertEquals(k.get(), 14);
-		k.get();
-		assertEquals(1, counter);
-	}
+    var k = i.flatMap(lazyDouble);
+    assertEquals(0, counter);
+    assertEquals(k.get(), 14);
+    k.get();
+    assertEquals(1, counter);
+  }
 }
+
