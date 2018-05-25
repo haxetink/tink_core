2c16228f8624de64905b29d9883bd7daf0b4558b
Evgeny Pavlovskiy <evgeny.pavlovskiy@doclerholding.com>
2018-05-25 15:51:38 +0200

    Fix `LazyConst`'s `map` and `flatMap` being eager.

--- src/tink/core/Lazy.hx
+++ src/tink/core/Lazy.hx
@@ -1,22 +1,22 @@
 package tink.core;
 
 abstract Lazy<T>(LazyObject<T>) from LazyObject<T> {
-  
+
   @:to public inline function get():T
     return this.get();
-      
-  @:from static public inline function ofFunc<T>(f:Void->T):Lazy<T> 
+
+  @:from static public inline function ofFunc<T>(f:Void->T):Lazy<T>
     return new LazyFunc(f);
 
-  public inline function map<A>(f:T->A):Lazy<A> 
+  public inline function map<A>(f:T->A):Lazy<A>
     return this.map(f);
-    
-  public inline function flatMap<A>(f:T->Lazy<A>):Lazy<A> 
+
+  public inline function flatMap<A>(f:T->Lazy<A>):Lazy<A>
     return this.flatMap(f);
-  
+
   @:from @:noUsing static inline function ofConst<T>(c:T):Lazy<T>
     return new LazyConst(c);
-}  
+}
 
 private interface LazyObject<T> {
   function get():T;
@@ -25,7 +25,7 @@ private interface LazyObject<T> {
 }
 
 private class LazyConst<T> implements LazyObject<T> {
-  
+
   var value:T;
 
   public inline function new(value)
@@ -35,19 +35,19 @@ private class LazyConst<T> implements LazyObject<T> {
     return value;
 
   public inline function map<R>(f:T->R):Lazy<R>
-    return new LazyConst(f(value));
+    return new LazyFunc(function () return f(value));
 
   public inline function flatMap<R>(f:T->Lazy<R>):Lazy<R>
-    return f(value);
+    return new LazyFunc(function () return f(value).get());
 }
 
 private class LazyFunc<T> implements LazyObject<T> {
   var f:Void->T;
   var result:T;
   #if debug var busy = false; #end
-  
+
   public function new(f) this.f = f;
-  
+
   public function get() {
     #if debug if (busy) throw new Error('circular lazyness');#end
     if (f != null) {
