363548c734fa7546538227db5826bb5bf0328550
Kevin Leung <kevinresol@gmail.com>
2018-09-24 10:45:07 +0800

    Sorry I'm idiot

--- src/tink/core/ReadOnlyArray.hx
+++ src/tink/core/ReadOnlyArray.hx
@@ -3,5 +3,4 @@ package tink.core;
 @:forward(length, concat, copy, filter, indexOf, iterator, join, lastIndexOf, map, slice, toString)
 abstract ReadOnlyArray<T>(Array<T>) from Array<T> {
 	@:arrayAccess public inline function get(i:Int) return this[i];
-	@:arrayAccess public inline function set(i:Int, v:T) return this[i] = v;
 }
