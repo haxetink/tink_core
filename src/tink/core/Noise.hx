c949fbe8e8e30061420b062201e2b4568b39eba9
Juraj Kirchheim <back2dos@googlemail.com>
2020-08-29 13:38:58 +0200

    Remove from cast of Never.

--- src/tink/core/Noise.hx
+++ src/tink/core/Noise.hx
@@ -2,8 +2,9 @@ package tink.core;
 
 enum abstract Noise(Null<Dynamic>) {
   var Noise = null;
-  @:from static inline function ofAny<T>(t:Null<T>):Noise
+  @:from static function ofAny<T>(t:Null<T>):Noise
     return Noise;
 }
 
-abstract Never(Noise) from Dynamic to Dynamic {}
+abstract Never(Noise) to Dynamic {
+}
