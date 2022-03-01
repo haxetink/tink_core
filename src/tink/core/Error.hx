ba035bb0db466e72956c3c36d6c7307dbc252db2
0b1kn00b <polysemantic@gmail.com>
2022-03-01 12:02:20 +0100

    Can't remember why the #if js

--- src/tink/core/Error.hx
+++ src/tink/core/Error.hx
@@ -80,10 +80,10 @@ class TypedError<T> {
     #end
     return ret;
   }
-  #if js
+
   public inline function toPromise<X>():Promise<X>
-    return Promise.lift((cast this:Error));
-  #end
+    return Promise.reject(cast this);
+
   public function throwSelf():Dynamic
     return
       #if macro
